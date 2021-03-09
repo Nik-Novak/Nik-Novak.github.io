5a35c1782a3dea67d3214f8bf02a6736d767a31e
Nik-Novak <novakn@mcmaster.ca>
2021-03-08 21:42:49 -0500

    latest version

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# nik-novak.github.io
+nik-novak.github.io
 CS 4WW3 CarPics Website
+
+
+Nik Novak
+001406602
+Nik-Novak.com OR CarPics.tk OR Novak-Nik.com OR Nik-Novak.com/4WW3
+(My domains are all near expiration so I will leave these three incase one or two go down. I don't plan to renew. Also CarPics.tk was having issues with EC2)
+
+
--- register/js/document.js
+++ register/js/document.js
@@ -1,19 +1,82 @@
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-        alert("Geolocation is not supported by this browser.");
+
+
+function formSubmit(){
+    console.log('wtf')
+    try{
+        var pass = true;
+        if($('#input_username').val().length < 5 || $('#input_title').val().length >15){
+            $('#input_username').css('border', '1px solid red');
+            $('#input_username').next('p').html('Username must be between 5 and 15 characters.');
+            $('#input_username').next('p').css('color','red');
+        }
+        else{
+            resetUser();
+            pass=false;
+        }
+        
+        if($('#input_email').val().split('.').length <=1){
+            $('#input_email').css('border', '1px solid red');
+            $('#input_email').next('p').html('Email must follow standard format.');
+            $('#input_email').next('p').css('color','red');
+        }
+        else{
+            resetEmail();
+            pass=false;
+        }
+        
+        if( $('#input_password').val().length < 7 || $('#input_password').val().length > 26 ){
+            $('#input_password').css('border', '1px solid red');
+            $('#input_password').next('p').html('Password must be between 7 and 26 characters.');
+            $('#input_password').next('p').css('color','red');
+        }
+        else{
+            resetPassword();
+            pass=false;
+        }
+        
+        if( $('#input_confirmpassword').val() != $('#input_password').val()){
+            $('#input_confirmpassword').css('border', '1px solid red');
+            $('#input_confirmpassword').next('p').html('Passwords do not match.');
+            $('#input_confirmpassword').next('p').css('color','red');
+        }
+        else{
+            resetConfirmPassword();
+            pass=false;
+        }
+        
+        alert(pass);
+        return pass;
+        
+        
+    }catch(e){
+        console.log(e.message);
+        return false;
     }
 }
 
-function initMap() {
-    var uluru = {lat: -25.363, lng: 131.044};
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 4,
-      center: uluru
-    });
-    var marker = new google.maps.Marker({
-      position: uluru,
-      map: map
-    });
+
+
+function resetUser(){
+    $('#input_username').css('border', '1px solid rgb(213, 219, 221)');
+    $('#input_username').next('p').html('');
+    $('#input_username').next('p').css('color','black');
 }
+
+function resetEmail(){
+    $('#input_email').css('border', '1px solid rgb(213, 219, 221)');
+    $('#input_email').next('p').html('');
+    $('#input_email').next('p').css('color','black');
+}
+
+function resetPassword(){
+    $('#input_password').css('border', '1px solid rgb(213, 219, 221)');
+    $('#input_password').next('p').html('');
+    $('#input_password').next('p').css('color','black');
+}
+
+function resetConfirmPassword(){
+    $('#input_confirmpassword').css('border', '1px solid rgb(213, 219, 221)');
+    $('#input_confirmpassword').next('p').html('');
+    $('#input_confirmpassword').next('p').css('color','black');
+}
+
