7f3f31b823ea83907680f1b6b8f7ccb1bb9674c1
Nik-Novak <novakn@mcmaster.ca>
2021-03-08 21:24:44 -0500

    added carpics to gh pages

---
+++ README.md
@@ -0,0 +1,2 @@
+# nik-novak.github.io
+CS 4WW3 CarPics Website
---
+++ register/js/document.js
@@ -0,0 +1,19 @@
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        alert("Geolocation is not supported by this browser.");
+    }
+}
+
+function initMap() {
+    var uluru = {lat: -25.363, lng: 131.044};
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 4,
+      center: uluru
+    });
+    var marker = new google.maps.Marker({
+      position: uluru,
+      map: map
+    });
+}
